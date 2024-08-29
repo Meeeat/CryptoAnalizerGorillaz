92171dc58ff0942c8c64551f0ad11c6e4594fb8c
Meeeat <angroov@gmail.com>
2024-08-29 20:58:49 +0900

    REF change FrequencyConstants and some fix

--- src/main/java/com/javarush/siberia/constants/FrequencyConstants.java
+++ src/main/java/com/javarush/siberia/constants/FrequencyConstants.java
@@ -22,12 +22,8 @@ public class FrequencyConstants {
 
     private static Map<Character, Double> createRussianFrequency() {
         Map<Character, Double> frequencyMap = new TreeMap<>();
-
-        // Массивы символов и их частот
         char[] characters = {'о', 'е', 'а', 'и', 'н', 'т', 'с', 'р', 'л', 'в', 'д', 'м', 'к', 'у', 'ы', 'з', 'ь', 'я', 'ч', 'г', 'б', 'ш', 'э', 'ю', 'ж', 'х', 'ц', 'щ', 'ф', 'ъ', ' '};
         double[] frequencies = {10.97, 8.45, 8.01, 7.34, 6.71, 6.60, 5.43, 4.81, 4.46, 4.43, 3.96, 3.79, 3.52, 3.47, 3.31, 2.86, 2.63, 2.54, 2.51, 2.46, 2.29, 1.96, 1.87, 1.59, 1.25, 1.14, 1.09, 0.89, 0.60, 0.25, 17.00};
-
-        // Заполнение карты с помощью цикла
         for (int i = 0; i < characters.length; i++) {
             frequencyMap.put(characters[i], frequencies[i]);
         }
